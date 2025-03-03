8bc20971276b07f5b67852f4e9c638954d3938d5
firat-sowft <goymenmhmd@gmail.com>
2025-03-03 11:17:08 +0300

    Initial commit

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def delete_word(word_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
